59152e7860d6dbe4c46635b96fbe41669778228d
antony <gitrepositoryphp@gmail.com>
2021-10-09 10:50:34 +0530

    update add new

--- create-component.ts
+++ create-component.ts
@@ -2,6 +2,7 @@ create-component.ts
 ---------------------------------------------
 employees:any
 employee:any
+id:any;
 constructor(private employeeService: EmployeeService,
     private router: Router) { }
 
@@ -63,3 +64,58 @@ getEmployee single Detail
 }
 ------------------------------------------------------------------------------------------------
 
+update
+----------------------------------------------------------------------------
+
+id:any;
+constructor(private route: ActivatedRoute, private employeeService: EmployeeService,
+  private router: Router) { }
+
+ngOnInit() {
+
+  this.id = this.route.snapshot.params['id'];
+
+  this.employeeService.getEmployeeById(this.id).subscribe(data => {
+
+    this.registerForm.setValue({
+      firstName: data.firstName,
+      lastName : data.lastName,
+      email : data.email,
+      password : data.password,
+      confirmPassword: data.confirmPassword
+  
+    })
+
+  },  error => console.log(error));
+
+
+}
+saveEmployee(){
+
+  const body = {
+    firstName: this.registerForm.get('firstName').value,
+    lastName: this.registerForm.get('lastName').value,
+    email: this.registerForm.get('email').value,
+    password: this.registerForm.get('password').value,
+    confirmPassword: this.registerForm.get('confirmPassword').value,
+
+  }
+
+
+if(this.id){
+
+//use update code
+
+
+}else{
+this.employeeService.createEmployee(body).subscribe( data =>{
+  console.log(data);
+ 
+},
+error => console.log(error));
+
+}
+
+  
+  
+}
